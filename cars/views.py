049e19525a5d4d7cd731ef191dd30c8a16abbc3e
fizarqq <kirillozubenko@gmail.com>
2023-08-13 21:43:07 +0300

    commit

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,84 @@
-from django.shortcuts import render
+from django.forms import model_to_dict
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.http import Http404
+from rest_framework import status
 
 from .models import CarModel
-from .serializers import CarSerializer,CarAllSerializer
+from .serializers import CarSerializer, CarAllSerializer
 
+
+class CarListCreateView(APIView):
+    def get(self, *args, **kwargs):
+        cars = CarModel.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        # res = [model_to_dict(car) for car in cars]
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def post(self, *args, **kwargs):
+        data = self.request.data
+        serializer = CarSerializer(data=data)
+        # if not serializer.is_valid():
+        #     return Response(serializer.errors)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        # car = CarModel(**data)
+        # car.save()
+
+        # car = CarModel.objects.create(**serializer.data)
+        # serializer = CarSerializer(car)
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
+
+class CarUpdateDestroyView(APIView):
+    def get(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        try:
+            car = CarModel.objects.get(pk=pk)
+        except CarModel.DoesNotExist:
+            raise Http404()
+        serializer = CarSerializer(car)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def put(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        data: dict = self.request.data
+        try:
+            car = CarModel.objects.get(pk=pk)
+        except CarModel.DoesNotExist:
+            raise Http404()
+        # for k, v in data.items():
+        #     setattr(car, k, v)
+        # car.save()
+        serializer = CarSerializer(car, data)
+        # if not serializer.is_valid():
+        #     return Response(serializer.errors)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def patch(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        data: dict = self.request.data
+        try:
+            car = CarModel.objects.get(pk=pk)
+        except CarModel.DoesNotExist:
+            raise Http404()
+
+        serializer = CarSerializer(car, data, partial=True)
+
+        # if not serializer.is_valid():
+        #     return Response(serializer.errors)
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def delete(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        try:
+            car = CarModel.objects.get(pk=pk)
+            car.delete()
+        except CarModel.DoesNotExist:
+            raise Http404()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- configs/urls.py
+++ configs/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-urlpatterns = [
+from cars.views import CarListCreateView, CarUpdateDestroyView
 
+urlpatterns = [
+    path('cars', CarListCreateView.as_view()),
+    path('cars/<int:pk>', CarUpdateDestroyView.as_view())
 ]
